b572a66aa13915a1633060810339ced15dad9958
ljjun <gwejja@gmail.com>
2020-07-06 15:46:25 +0900

    Add: 초기 설정

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Main from "Pages/Main";
+import Home from "Pages/Home";
+import Pin from "Pages/Pin";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/pin/:id" exact component={Pin} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import Routes from "Routes";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *{
+    padding: 0px;
+    margin: 0px;
+    box-sizing:border-box;
+  }
+  a{
+    text-decoration:none;
+    color: inherit;
+    cursor:pointer;
+  }
+  ol,ul{
+    list-style:none;
+  }
+  body{
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Fira Sans","Droid Sans",Helvetica Neue,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+    font-size: 16px;
+    color: #111111;
+  }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Fragment>
+    <GlobalStyle />
+    <Routes />
+  </Fragment>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
